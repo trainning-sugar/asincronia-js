57d7273fdd05b37975d488c1668a50d3960d0d46
trainning-sugar <mlvilchezp@gmail.com>
2018-07-24 20:01:14 -0500

    intento de callback

---
+++ callback/app1.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('button');
+
+const obj = {
+  '+' : (x,y) => x + y,
+  '-' : (x,y) => x - y,
+  '*' : (x,y) => x * y,
+  '/' : (x,y) => x / y
+}
+
+const doProcess = (num1, num2, string) => {
+    console.log(obj[string](num1,num2));
+}
+
+/* const doProcess = (num1, num2, type) => {
+  if (type === '+') {
+    alert(num1 + num2);
+  } else if (type === '-') {
+    alert(num1 - num2)
+  } else if (type === 'x') {
+    alert(num1 * num2);
+  } else {
+    alert(num1 / num2);
+  }
+} */
+
+const clickHandler = () => {
+  const a = Number(prompt('Ingrese un numero'));
+  const b = Number(prompt('Ingrese otro numero'));
+  const string = prompt('Ingrese la operacion +,-,x o /');
+  doProcess(a, b, string);
+}
+
+
+button.addEventListener('click', clickHandler);
