7a64268add770d7f694f011492718088f3deb3cd
trainnig-sugar <mlvilchezp@gmail.com>
2018-07-27 17:03:10 -0500

    testeando el código asíncrono de las imagenes

--- callback/app3.js
+++ callback/app3.js
@@ -1,9 +1,11 @@
-/*Cuando esto este listo ejecuta ese pedazo de código --> Callback*/
+/*Callbacks Cuando esto este listo, y yo no sé cuando lo esté, ejecuta ese pedazo de código --> Callback*/
 
 const loadImg = (url, cb) => {
+ console.log('me estas leyedo');
  const img = new Image();
 
  img.onload = () => {
+  console.log(img.src);
   cb(null, img);
  };
  img.onerror = () => {
@@ -11,6 +13,7 @@ const loadImg = (url, cb) => {
  };
 
  img.src = url;
+ console.log('me terminé de cargar')
 }
 const printImage = (src) => {
  const imgElement = document.createElement('img');
@@ -19,9 +22,8 @@ const printImage = (src) => {
 }
 
 //console.log(loadImg('images/1.jfif').src);
-
-loadImg('https://ugc.kn3.net/i/origin/http://farm2.static.flickr.com/1258/953669278_349a6a9897_o.jpg', (err, img) => {
- if (err) throw err;
+loadImg('images/1.jfif', (err, img) => {
+ if (err) throw console.error(err);
  printImage(img.src);
  loadImg('https://sergimateo.com/wp-content/2012/08/fotos-panoramicas-4.jpg', (err, img) => {
   if (err) throw err;
