059f3d5affb247db042e68967d1d40001f59ac58
trainnig-sugar <mlvilchezp@gmail.com>
2018-07-27 15:58:39 -0500

    agregando imagenes super pesadas para probar la asicnronia

--- callback/app3.js
+++ callback/app3.js
@@ -1,12 +1,17 @@
 /*Cuando esto este listo ejecuta ese pedazo de código --> Callback*/
 
-const loadImg = (url) => {
+const loadImg = (url, cb) => {
  const img = new Image();
- img.src = url;
- console.log(img);
- return img
-}
 
+ img.onload = () => {
+  cb(null, img);
+ };
+ img.onerror = () => {
+  cb(new Error(`La imagen no pudo cargarse ${url}`))
+ };
+
+ img.src = url;
+}
 const printImage = (src) => {
  const imgElement = document.createElement('img');
  imgElement.src = src;
@@ -15,6 +20,19 @@ const printImage = (src) => {
 
 //console.log(loadImg('images/1.jfif').src);
 
-printImage(loadImg('images/1.jfif').src)
-printImage(loadImg('images/2.jfif').src)
-printImage(loadImg('images/3.jfif').src)
+loadImg('https://ugc.kn3.net/i/origin/http://farm2.static.flickr.com/1258/953669278_349a6a9897_o.jpg', (err, img) => {
+ if (err) throw err;
+ printImage(img.src);
+ loadImg('https://sergimateo.com/wp-content/2012/08/fotos-panoramicas-4.jpg', (err, img) => {
+  if (err) throw err;
+  printImage(img.src);
+  loadImg('https://sergimateo.com/wp-content/2012/08/fotos-panoramicas-10.jpg', (err, img) => {
+   if (err) throw err;
+   printImage(img.src);
+   loadImg('https://sergimateo.com/wp-content/2012/08/fotos-panoramicas-15.jpg', (err, img) => {
+    if (err) throw err;
+    printImage(img.src);
+   })
+  })
+ })
+})
